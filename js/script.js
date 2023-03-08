33fb644f297f26c1af990f1f69c204169f0ae423
marianitadileo <mdileo36@gmail.com>
2023-03-08 18:46:07 +0100

    add style css

--- js/script.js
+++ js/script.js
@@ -16,13 +16,13 @@ if(ageUser < 18) {
     reducedPrice = priceJourney - discountJunior; 
     reducedPrice = reducedPrice.toFixed(2); 
     console.log(reducedPrice);
-    document.getElementById('price').innerHTML = "Il costo del biglietto sarà " + reducedPrice + "€";
+    document.getElementById('price').innerHTML = "Buone notizie! Avete ricevuto uno sconto del 20%. Il costo del biglietto sarà " + reducedPrice + "€";
 } else if (ageUser > 65) {
     discountOver = 0.4;
     reducedPrice = priceJourney - discountOver;
     reducedPrice = reducedPrice.toFixed(2); 
     console.log(reducedPrice);
-    document.getElementById('price').innerHTML = "Il costo del biglietto sarà " + reducedPrice + "€";
+    document.getElementById('price').innerHTML = "Buone notizie! Avete ricevuto uno sconto del 40%. Il costo del biglietto sarà " + reducedPrice + "€";
 } else {
     fullPrice = "Spiacenti, non è previsto nessuno sconto";
     console.log(priceJourney);
