a14ffe70eb2963c8e2f8a03652c0631df8a0124e
marianitadileo <mdileo36@gmail.com>
2023-03-08 15:49:27 +0100

    price journey

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const kmJourney = parseInt(prompt("Quanti chilometri dovrai percorrere?"));
+const ageUser = parseInt(prompt("Quanti anni hai?"));
+console.log(kmJourney, ageUser);
+
+const priceKm = 0.21
+
+const priceJourney = kmJourney * priceKm;
+console.log(priceJourney);
+
+
